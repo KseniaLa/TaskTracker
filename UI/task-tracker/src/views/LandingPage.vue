6656553b7f23b30778242f9863a6059701bde078
Ksenia Lashchinovskaya <ksLa1998@mail.ru>
2019-08-27 14:05:05 +0300

    add select component

--- UI/task-tracker/src/views/LandingPage.vue
+++ UI/task-tracker/src/views/LandingPage.vue
@@ -1,23 +1,36 @@
 <template>
 	<main class="landing-page-container">
 		Landing page
+		<label class="typo__label">Select application mode</label>
+		<multiselect v-model="value" :options="options" :searchable="false" :close-on-select="true" :show-labels="false" placeholder="Pick a value"></multiselect>
+		<pre class="language-json"><code>{{ value  }}</code></pre>
 		<router-link to="/main">Go to MainPage</router-link>
 	</main>
     
 </template>
 
 <script>
+import Multiselect from "vue-multiselect";
+
 export default {
-    
-}
+  components: {
+    Multiselect
+  },
+  data() {
+    return {
+      value: "",
+      options: ["Demo", "Real"]
+    };
+  }
+};
 </script>
 
 <style lang="scss">
 .landing-page-container {
-	display: flex;
-	flex-direction: column;
-	background: #005eda;
-	height: 100%;
+  display: flex;
+  flex-direction: column;
+  background: #005eda;
+  height: 100%;
 }
 </style>
 
