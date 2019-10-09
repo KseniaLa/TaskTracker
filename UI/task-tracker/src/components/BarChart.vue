0239d37bd836f6126cd50cd6f7f7ce1fe701a94d
Ksenia Lashchinovskaya <ksLa1998@mail.ru>
2019-10-09 12:54:59 +0300

    add chart (test)

---
+++ UI/task-tracker/src/components/BarChart.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="small">
+    <bar-chart :chart-data="datacollection"></bar-chart>
+    <button @click="fillData()">Randomize</button>
+  </div>
+</template>
+
+<script>
+  import BarChart from './BarChart.js'
+
+  export default {
+    components: {
+      BarChart
+    },
+    data () {
+      return {
+        datacollection: null
+      }
+    },
+    mounted () {
+      this.fillData()
+    },
+    methods: {
+      fillData () {
+        this.datacollection = {
+          labels: [this.getRandomInt(), this.getRandomInt()],
+          datasets: [
+            {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }, {
+              label: 'Data One',
+              backgroundColor: '#f87979',
+              data: [this.getRandomInt(), this.getRandomInt()]
+            }
+          ]
+        }
+      },
+      getRandomInt () {
+        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+      }
+    }
+  }
+</script>
+
+<style>
+  .small {
+    width: 60px;
+  }
+</style>
--- UI/task-tracker/src/components/Dashboard.vue
+++ UI/task-tracker/src/components/Dashboard.vue
@@ -2,37 +2,57 @@
 	<div class="dashboard">
 	  <div class="section-header">
 			<span>Dashboard</span>
-			
 			<button class="add-button" v-on:click="openModal">Add</button>
-			<modal name="widget" height="90%" width="50%">
-      	<WidgetModal />
-    	</modal>
+		</div>
+		<div class="chart-area">
+			<bar-chart></bar-chart>
 		</div>
 
+		<modal name="widget" height="90%" width="50%">
+      <WidgetModal />
+    </modal>
   </div>
 </template>
 
 <script>
 import WidgetModal from "./AddWidgetModal";
+import BarChart from "./BarChart";
 export default {
-	name: "Dashboard",
-	components: {
-		WidgetModal
-	},
-	methods: {
-    openModal: function() {
-      this.$modal.show('widget');
-    }
+  name: "Dashboard",
+  components: {
+		WidgetModal,
+		BarChart
   },
+  methods: {
+    openModal: function() {
+      this.$modal.show("widget");
+    }
+  }
 };
 </script>
 
 <style lang="scss">
 .dashboard {
-	display: flex;
-	flex-direction: column;
-	width: 60%;
-	border-right: solid 1px #a8a8a8;
-	height: 100%;
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  border-right: solid 1px #a8a8a8;
+  height: 100%;
+}
+
+.chart-area {
+  overflow: hidden;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    background-color: #f5f5f5;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #555;
+  }
 }
 </style>
