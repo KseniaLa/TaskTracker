768d6b102368a6915ee783b8c50c03f999a62951
KseniaLa <ksla1998@mail.ru>
2019-10-24 18:09:24 +0300

    add api call engine and CORS

--- UI/task-tracker/src/dataWorker/ApiWorker.js
+++ UI/task-tracker/src/dataWorker/ApiWorker.js
@@ -1,8 +1,13 @@
+import axios from "axios";
+
 class ApiWorker {
   constructor() {}
 
   addTask() {
     console.log("I request api for task!");
+    axios
+      .get("http://localhost:52779/api/task")
+      .then(response => console.log(response));
   }
 
   addWidget() {
