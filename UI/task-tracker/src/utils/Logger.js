6d389004bd2ff77656662c5ffcc2985039e87653
KseniaLa <ksla1998@mail.ru>
2019-11-16 00:35:22 +0300

    add some methods to logger

--- UI/task-tracker/src/utils/Logger.js
+++ UI/task-tracker/src/utils/Logger.js
@@ -8,6 +8,18 @@ class Logger {
       this._log.info(message);
     }
   }
+
+  warn(isServerConnected, message) {
+    if (isServerConnected) {
+      this._log.warn(message);
+    }
+  }
+
+  error(isServerConnected, message) {
+    if (isServerConnected) {
+      this._log.error(message);
+    }
+  }
 }
 
 export default Logger;
