01ca38ed397773ff6746d225a4e0bb2bb4e47a28
KseniaLa <ksla1998@mail.ru>
2020-01-12 14:32:38 +0300

    add DashboardMicroservice

---
+++ Server/DashboardMicroservice/DashboardMicroservice/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace DashboardMicroservice
+{
+     public class Startup
+     {
+          public Startup(IHostingEnvironment env)
+          {
+               var builder = new Microsoft.Extensions.Configuration.ConfigurationBuilder();
+               builder.SetBasePath(env.ContentRootPath)
+                      //add configuration.json  
+                      .AddJsonFile("configuration.json", optional: false, reloadOnChange: true)
+                      .AddEnvironmentVariables();
+
+               Configuration = builder.Build();
+          }
+
+          public IConfigurationRoot Configuration { get; }
+
+          // This method gets called by the runtime. Use this method to add services to the container.
+          public void ConfigureServices(IServiceCollection services)
+          {
+               services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+               services.AddOcelot(Configuration);
+          }
+
+          // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+          public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
+          {
+               if (env.IsDevelopment())
+               {
+                    app.UseDeveloperExceptionPage();
+               }
+
+               app.UseMvc();
+
+               await app.UseOcelot();
+          }
+     }
+}
